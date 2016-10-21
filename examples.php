891d84e1a0f2c2e660f52aaf4fa28d57997949a6
David Richied <davidrichied@gmail.com>
2016-10-21 20:09:53 +0000

    finished front page

---
+++ examples.php
@@ -0,0 +1,110 @@
+
+
+
+
+
+
+
+
+
+<div class="tire-landing-page-header">
+	
+<img src="/wp-content/uploads/2016/10/Screen-Shot-2016-10-21-at-2.51.08-PM.png" alt="screen-shot-2016-10-21-at-2-51-08-pm" width="1440" height="437" class="alignnone size-full wp-image-262" />
+</div>
+<div class="tire-landing-top">
+	<div class="tire-landing-logo">
+		<img class="alignnone size-medium wp-image-268" src="/wp-content/uploads/2016/10/BFGoodrichLandingPageExample-21-300x200.png" alt="bfgoodrichlandingpageexample-21" width="300" height="200" />
+	</div>
+
+	<div class="tire-landing-desc">
+		<h3>BFGoodrich® tires —</h3>
+		<h4>Are you driver enough?</h4>
+		<p>Passion, pride and performance are BFGoodrich values that have stood the test of time. As a maker of superior performance tires, we’re proud to say that we’ve been authentically performance-driven since the start. From the deserts of Baja to the road track at Laguna Seca, our rich heritage of successful performance racing ensures that your vehicle’s ready for any challenge — on any terrain. That is, if you’re up to the task.</p>
+	</div>
+</div>
+
+<div class="tire-lp-vids">
+	<div class="ft-one-third">
+		<div class="tire-post-image" style="background-image: url('/wp-content/uploads/2016/10/BFGoodrichLandingPageExample-7-300x162.png');"></div>
+			<a href="#"><h3>ARE YOU DRIVER ENOUGH?</h3></a>
+			<p>Now it’s the pros' turn. With the top 10 chosen, @Brad Lovell and @Andrew Comrie-Picard give their on-road and off-road best to set the pace for the rest of the Driver Enough Challenge. See how they perform.</p>
+	</div>
+
+
+
+	<div class="ft-one-third">
+		<div class="tire-post-image" style="background-image: url('/wp-content/uploads/2016/10/BFGoodrichLandingPageExample-6-300x159.png');"></div>
+			<a href="#"><h3>KO2 TAKES ON GRAVITY</h3></a>
+			<p>The new All-Terrain T/A® KO2 is our toughest all-terrain tire ever. Tough enough to take on nearly any challenge on Playground EarthTM.</p>
+	</div>
+
+
+	<div class="ft-one-third">
+		<div class="tire-post-image" style="background-image: url('/wp-content/uploads/2016/10/BFGoodrichLandingPageExample-5-300x158.png');"></div>
+			<a href="#"><h3>BFGOODRICH® G-FORCETM COMP-2TM A/S</h3></a>
+			<p>Calling the COMP-2TM A/S a serious ultra-high performance tire is an understatement. See what makes this the perfect tire for performance-seekers who crave a thrilling drive.</p>
+	</div>
+</div>
+
+
+
+
+
+
+<div class="bfg-lp featured-tires">
+	<h2>FEATURED TIRES</h2>
+	<div class="tire-row">
+		<div class="ft-tire-row-image">
+			<img src="/wp-content/uploads/2016/10/Screen-Shot-2016-10-21-at-2.41.41-PM-300x289.png" alt="screen-shot-2016-10-21-at-2-41-41-pm" width="300" height="289" class="alignnone size-medium wp-image-255" />
+		</div>
+
+		<div class="ft-tire-row-text">
+			<h3>ALL-TERRAIN T/A® KO2</h3>
+			<p>From $172.00 per tire<p>
+			<p>BFGoodrich's toughest all-terrain tire. Ever.</p>
+		</div>
+
+		<div class="ft-tire-row-lev">
+			<div class="tire-post-image" style="background-image: url('/wp-content/uploads/2016/10/Screen-Shot-2016-10-21-at-2.43.50-PM-300x221.png');"></div>
+		</div>
+	</div>
+
+
+	<div class="tire-row">
+		<div class="ft-tire-row-image">
+			<img src="/wp-content/uploads/2016/10/Screen-Shot-2016-10-21-at-2.42.01-PM-300x258.png" alt="screen-shot-2016-10-21-at-2-42-01-pm" width="300" height="258" class="alignnone size-medium wp-image-256" />
+		</div>
+
+		<div class="ft-tire-row-text">
+			<h3>G-FORCETM SPORT COMP-2TM</h3>
+			<p>From $95.00 per tire<p>
+			<p>Accelerate faster1, corner harder2 & brake shorter3 for next-level control and next-level fun on the street.</p>
+		</div>
+
+		<div class="ft-tire-row-lev">
+			<div class="tire-post-image" style="background-image: url('/wp-content/uploads/2016/10/Screen-Shot-2016-10-21-at-2.44.01-PM-300x233.png');"></div>
+		</div>
+	</div>
+
+	<div class="tire-row">
+		<div class="ft-tire-row-image">
+			<img src="/wp-content/uploads/2016/10/Screen-Shot-2016-10-21-at-2.42.21-PM-300x265.png" alt="screen-shot-2016-10-21-at-2-42-21-pm" width="300" height="265" class="alignnone size-medium wp-image-257" />
+		</div>
+
+		<div class="ft-tire-row-text">
+			<h3>G-FORCETM COMP-2TM A/S</h3>
+			<p>From $111.00 per tire<p>
+			<p>BFGoodrich's best ultra-high performance all-season tire ever.</p>
+		</div>
+
+		<div class="ft-tire-row-lev">
+			<div class="tire-post-image" style="background-image: url('/wp-content/uploads/2016/10/Screen-Shot-2016-10-21-at-2.44.01-PM-300x233.png');"></div>
+		</div>
+	</div>
+
+</div>
+
+
+<div class="tlp-bottom-logo">
+	<img class="alignnone size-medium wp-image-261" src="/wp-content/uploads/2016/10/Screen-Shot-2016-10-21-at-2.48.59-PM-300x97.png" alt="screen-shot-2016-10-21-at-2-48-59-pm" width="300" height="97" />
+</div>
